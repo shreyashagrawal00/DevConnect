221f8f501b57997d00ef4d6246e408ef32606707
Shreyash Agrawal <shreyash.agrawal3004@gmail.com>
2026-04-28 14:45:12 +0530

    Initial commit with DevConnect fullstack platform

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import CreatePost from './pages/CreatePost';
+import { AuthProvider } from './context/AuthContext';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <main className="container" style={{ paddingTop: '2rem', paddingBottom: '4rem' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/create" element={<CreatePost />} />
+          </Routes>
+        </main>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
